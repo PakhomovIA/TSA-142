6ee00dea7f20948de46655fb57f764a687499529
PakhomovIA <160127085+PakhomovIA@users.noreply.github.com>
2024-06-10 01:06:15 +0300

    Update 3Task.cpp

--- 3Task.cpp
+++ 3Task.cpp
@@ -1,29 +1,24 @@
 #include <iostream>
-
 using namespace std;
-
 /**
  * @brief Проверяет, является ли число натуральным.
  * @param num Целое число для проверки.
  * @return true, если число является натуральным (больше нуля), в противном случае - false.
  */
-bool isNatural(const int num);
-
+bool isNatural(int num);
 /**
  * @brief Проверяет, является ли число целым.
  * @param num Число типа float для проверки.
  * @return true, если число является целым, в противном случае - false.
  */
-bool isInteger(const float num);
-
+bool isInteger(float num);
 /**
  * @brief Вычисляет среднее арифметическое двух целых чисел.
  * @param num1 Первое целое число для вычисления среднего.
  * @param num2 Второе целое число для вычисления среднего.
  * @return Среднее арифметическое чисел num1 и num2 в виде числа типа float.
  */
-float calculateAverage(const int num1,const int num2);
-
+float calculateAverage(int num1, int num2);
 /**
  * @brief Основная функция программы.
  * @return Возвращает целое число, указывающее на успешное завершение программы (0).
@@ -32,24 +27,16 @@ int main()
 {
     setlocale(LC_ALL, "Russian");
     int num1, num2;
-
     cout << "Введите первое натуральное число: ";
     cin >> num1;
 
-    if (!isNatural(num1))
-    {
-        cout << "Ошибка: Введите натуральное число." << endl;
-        return 1;
-    }
+    bool isNatural(int num1);
+
 
     cout << "Введите второе натуральное число: ";
     cin >> num2;
 
-    if (!isNatural(num2))
-    {
-        cout << "Ошибка: Введите натуральное число." << endl;
-        return 1;
-    }
+    bool isNatural(int num2);
 
     float average = calculateAverage(num1, num2);
 
@@ -61,23 +48,18 @@ int main()
     {
         cout << "Среднее арифметическое чисел " << num1 << " и " << num2 << " не является целым числом." << endl;
     }
-
     return 0;
 }
-
-bool isNatural(const int num)
+bool isNatural(int num)
 {
     return num > 0;
 }
-
-bool isInteger(const float num)
+bool isInteger(float num)
 {
     int intPart = static_cast<int>(num);
     return num == intPart;
 }
-
-
-float calculateAverage(const int num1, const  int num2)
+float calculateAverage(int num1, int num2)
 {
     return (num1 + num2) / 2.0;
 }
